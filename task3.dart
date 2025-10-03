b1f46d28c1c35be1c92cd702bb4ca2cdea8bcdba
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-03 09:51:06 +0500

    doing task3

---
+++ task3.dart
@@ -0,0 +1,24 @@
+//Task3.1
+// Tomonlari mos ravishda 23 va 14 bo`lgan to`g`ri to`rt burchakni  perimeteri va yuzini toping.
+// void main() {
+//   double a = 23;
+//   double b = 14;
+//   double perimeter = 2 * (a + b);
+//   double area = a * b;
+//   print("Perimeter: $perimeter");
+//   print("Area: $area");
+// }
+
+//Task3.2
+// Berilgan 3,7,11,23,5 tub sonlarining o`rtacha qiymatini toping.
+
+void main() {
+  var son1, son2, son3, son4, son5;
+  son1 = 3;
+  son2 = 7;
+  son3 = 11;
+  son4 = 23;
+  son5 = 5;
+  var ortaQiymat = (son1 + son2 + son3 + son4 + son5) / 5;
+  print(ortaQiymat);
+}
