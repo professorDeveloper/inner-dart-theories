a8a649806655438d453a9d49e3ca9e15cfc1a07a
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-06 17:16:48 +0500

    fkc updated

--- lessons/lesson4.dart
+++ lessons/lesson4.dart
@@ -1,5 +1,17 @@
-void main() {
-  for (int i = 1; true; i++) {
-    print(i);
-  }
-}
+// void main() {
+//   for (int i = 1; true; i++) {
+//     print(i);
+//   }
+// }
+
+// void main() {
+//   var str = "Hello world";
+//   print(str.startsWith("Hello world"));
+// }
+
+// void main() {
+//   var str1 = "Hello";
+//   var str2 = "Helloo";
+
+//   print(str1.compareTo(str2));
+// }
