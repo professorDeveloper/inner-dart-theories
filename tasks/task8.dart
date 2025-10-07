a298811a6650fb1e616936a91c4bad6c659be9ab
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-07 09:23:48 +0500

    all tasks done

---
+++ tasks/task8.dart
@@ -0,0 +1,7 @@
+// Masal1 : listning o`rtadagi itemni 0 bilan alishtiring.
+// void main() {
+//   var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+//   var midIndex = list.length ~/ 2;
+//   list[midIndex] = 0;
+//   print(list);
+// }
