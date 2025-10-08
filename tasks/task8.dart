1a7cb2a15e7e45f5da29e47813f322a8becb97a5
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-08 09:27:49 +0500

    task8.2 done

--- tasks/task8.dart
+++ tasks/task8.dart
@@ -5,3 +5,12 @@
 //   list[midIndex] = 0;
 //   print(list);
 // }
+
+//Masala2 using map
+void main() {
+  var map ={"user_id":1001,"user_password":"herewego123"};
+  var data = map["user_password"];
+  map.remove("user_password");
+  map["user_pwd"] = data??"";
+  print(map);
+}
