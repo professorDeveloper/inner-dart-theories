6ebfbbae58d3f872e991b5214f6ebeb29b63f8f6
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-08 09:53:13 +0500

    task9.1 & task9.2 done

---
+++ tasks/task9.dart
@@ -0,0 +1,16 @@
+//Task1
+// void main(){
+//   var name ="Saikou Azamov";
+//   var unicodeList = name.codeUnits;
+//   print(unicodeList);
+// }
+
+//Task2
+// 97,98,99...121,122 unicodlari berilgan bo`lsa bu ketma-ketlikni aslini toping
+// void main(){
+//   var name = "";
+//   for (var i = 97; i <= 122; i++) {
+//     name += String.fromCharCode(i);}
+//   print(name);
+//   }
+
