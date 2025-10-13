3c45f92b6e4954c6e8eda0697cf184132d6d1b9c
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-13 16:19:20 +0500

    task10 done

--- tasks/task10.dart
+++ tasks/task10.dart
@@ -1,18 +1,23 @@
 // Task10.1
 /*
 * Task1:  Peak Element
-Berilgan List da “Peak Element” ni chiqaradigan funksiya yozing.
-* “Peak Element” deyiladi qachonki listdagi biror bir element undan oldin va keyin kelgan elementlardan bir vaqtda katta bo`lsa.
+* Berilgan List da “Peak Element” ni chiqaradigan funksiya yozing.
+* “Peak Element” deyiladi qachonki listdagi biror bir element undan oldin va keyin kelgan
+* elementlardan bir vaqtda katta bo`lsa.
 * */
-//
-// void main() {
-//   var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-//   for (var i = 0; i < list.length; i++) {
-//     if (list[i] > list[i - 1] && list[i] > list[i + 1]) {
-//       print(list[i]);
-//     }
-//   }
-// }
+
+void main() {
+  var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  printPeakElement(list);
+}
+
+void printPeakElement(List list) {
+  for (var i = 1; i < list.length - 1; i++) {
+    if (list[i] > list[i - 1] && list[i] > list[i + 1]) {
+      print(list[i]);
+    }
+  }
+}
 
 /*
  * Task2: Fixed Point
@@ -41,7 +46,7 @@ Berilgan List da “Peak Element” ni chiqaradigan funksiya yozing.
 * Shu “Missing Number” ni qaytaradigan funksiya yozing.
 * Missing Number” deyiladi qachonki listda u raqamdan boshqa hamma raqam mavjud bo`lsa.
 * */
-
+//
 // void main() {
 //   var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 //   print(findMissingNumber(list));
@@ -61,7 +66,8 @@ Berilgan List da “Peak Element” ni chiqaradigan funksiya yozing.
 * Ya`ni barcha  elementlari o`sish yoki kamayish tartibida joylashgan list monotonic list deyiladi.
 * Masalan: [1,3,5,6,7,8] yoki [12,10,6,4,3]
 * */
-// void main(){
+//
+// void main() {
 //   var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 //   print(isMonotonic(list));
 // }
@@ -86,16 +92,34 @@ Berilgan List da “Peak Element” ni chiqaradigan funksiya yozing.
 * o’sish tartibida joylashtirilgan
 * List hosil qiladigan funksiya yozing.
 * */
+//
+// void main() {
+//   var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+//   print(squaresOfSortedArray(list));
+// }
+//
+// List<int> squaresOfSortedArray(List<int> list) {
+//   List<int> result = [];
+//   for (var i = 0; i < list.length; i++) {
+//     result.add(list[i] * list[i]);
+//   }
+//   return result;
+// }
 
-void main() {
-  var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-  print(squaresOfSortedArray(list));
-}
+// New Tasks
+/*
+Task1: Add Strings
+Input: “111”, “222”
+Output: “333”
+*/
+// void main() {
+//   var str1 = "111";
+//   var str2 = "222";
+//   var sum = int.parse(str1) + int.parse(str2);
+//   print(sum);
+// }
 
-List<int> squaresOfSortedArray(List<int> list) {
-  List<int> result = [];
-  for (var i = 0; i < list.length; i++) {
-    result.add(list[i] * list[i]);
-  }
-  return result;
-}
+/*
+Task2: Find Complement
+Input: “110001”
+Output: “001110*/
